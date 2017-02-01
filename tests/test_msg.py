52f38e2a3bbc0b1001924b292206f694edb8d426
Kevin Barnes <kebdev@gmail.com>
2017-02-01 16:01:41 -0600

    Added pytests for regression testing; moved unit tests from msg.py to test_msg.py

--- tests/test_msg.py
+++ tests/test_msg.py
@@ -3,6 +3,88 @@ __author__ = 'kevin'
 from ..msg import extract_content
 
 
+# activate the environment and cd to tests/
+# run this command:
+#   py.test test_msg.py
+
+# start of pytest tests for regression
+def test_mentions():
+    msg = '@chris you around?'
+    actual = extract_content(msg)
+    expected = """{
+  "mentions": [
+    "chris"
+  ]
+}"""
+    assert(actual == expected)
+
+
+def test_emoticons():
+    msg = 'Good morning! (megusta) (coffee)'
+    actual = extract_content(msg)
+    expected = """{
+  "emoticons": [
+    "megusta",
+    "coffee"
+  ]
+}"""
+    assert(actual == expected)
+
+
+def test_links():
+    msg = 'Olympics are starting soon; http://www.nbcolympics.com'
+    actual = extract_content(msg)
+    expected = """{
+  "links": [
+    {
+      "url": "http://www.nbcolympics.com",
+      "title": "2018 PyeongChang Olympic Games | NBC Olympics"
+    }
+  ]
+}"""
+    assert(actual == expected)
+
+
+def test_all():
+    msg = '@bob @john (success) such a cool feature; https://twitter.com/jdorfman/status/430511497475670016'
+    actual = extract_content(msg)
+    expected = """{
+  "mentions": [
+    "bob",
+    "john"
+  ],
+  "emoticons": [
+    "success"
+  ],
+  "links": [
+    {
+      "url": "https://twitter.com/jdorfman/status/430511497475670016",
+      "title": "Justin Dorfman on Twitter: &quot;nice @littlebigdetail from @HipChat (shows hex colors when pasted in chat). http://t.co/7cI6Gjy5pq&quot;"
+    }
+  ]
+}"""
+    assert(actual == expected)
+
+
+def test_dup_links():
+    msg = 'Hello https://www.google.com and https://www.google.com'
+    actual = extract_content(msg)
+    expected = """{
+  "links": [
+    {
+      "url": "https://www.google.com",
+      "title": "Google"
+    },
+    {
+      "url": "https://www.google.com",
+      "title": "Google"
+    }
+  ]
+}"""
+    assert(actual == expected)
+
+
+# start of inline unit tests
 def run_test(msg):
     content = extract_content(msg)
     print content
@@ -18,10 +100,10 @@ def run_tests():
     run_test('Hello https://www.google.com and https://www.google.com')  # test caching
     return
 
-# cd to directory above hipchat
+# cd to directory above hiptest
 # ensure that environment is activated
 # run the command:
-#    python -m hipchat.tests.test_msg
+#    python -m hiptest.tests.test_msg
 if __name__ == '__main__':
     run_tests()
 
